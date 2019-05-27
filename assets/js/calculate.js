caac8d71a86422d3913f34e044b861677241f7d7
Samuel Grayson Eddy II <sgeddy@ncsu.edu>
2019-05-26 20:30:40 -0400

    Update calculate.js

--- assets/js/calculate.js
+++ assets/js/calculate.js
@@ -36,7 +36,7 @@ function deleteRow(tableID) {
     for(var i=1; i<rowCount; i++) {
         var row = table.rows[i];
         console.log(row.cells[0].childNodes);
-        var chkbox = row.cells[0].childNodes[8];
+        var chkbox = row.cells[0].childNodes[7];
         if(chkbox.checked == true) {
             if(rowCount <= 2) {
                 alert("Why would you want to delete all the rows?");
